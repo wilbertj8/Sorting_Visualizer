0db023d5e98750ffb1f0ea66c1b410c454faa309
wilbertj8 <wilbertjoseph08@gmail.com>
2022-08-04 19:30:40 -0400

    First commit

---
+++ src/visualizer/__init__.py
@@ -0,0 +1,27 @@
+import random, pygame
+pygame.init()
+
+class DrawInformation:
+    BLACK = 0, 0, 0
+    WHITE = 255, 255, 255
+    GREEN = 0, 255, 0
+    RED = 255, 0, 0
+    GREY = 128, 128, 128
+    BACKGROUND_COLOR = WHITE
+    
+    SIDE_PAD = 100
+    
+    def __init__(self, width, height, lst):
+        self.width = width
+        self.height = height
+        
+        self.window = pygame.display.set_mode((width, height))
+        pygame.display.set_caption('Sorting Algorithm Visualization')
+        self.set_list(lst)
+    
+    def set_list(self, lst):
+        self.lst = lst
+        self.min_val = min(lst)
+        self.max_val = max(lst)
+        
+        self.block_width = round((self.width - self.SIDE_PAD) / len(lst))
