b1cdd67008eb49954a152a03777914c527c99671
wilbertj8 <wilbertjoseph08@gmail.com>
2022-08-06 12:05:44 -0400

    added draw list

--- src/visualizer/__init__.py
+++ src/visualizer/__init__.py
@@ -10,6 +10,12 @@ class DrawInformation:
     GREY = 128, 128, 128
     BACKGROUND_COLOR = WHITE
     
+    GRADIENTS = [
+        (128, 128, 128),
+        (160, 160, 160),
+        (192, 192, 192)
+    ]
+    
     SIDE_PAD = 100
     TOP_PAD = 150
     
@@ -32,10 +38,19 @@ class DrawInformation:
 
 def draw(draw_info):
     draw_info.window.fill(draw_info.BACKGROUND_COLOR)
+    draw_list(draw_info)
     pygame.display.update()
 
 def draw_list(draw_info):
-    pass
+    lst = draw_info.lst
+    
+    for i, val in enumerate(lst):
+        x = draw_info.start_x + i * draw_info.block_width
+        y = draw_info.height - (val - draw_info.min_val) * draw_info.block_height
+        
+        color = draw_info.GRADIENTS[i%3]
+        pygame.draw.rect(draw_info.window, color, (x, y, draw_info.block_width, draw_info.block_height * val))
+        
 
 def main():
     run = True
