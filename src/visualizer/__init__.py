1ff3aa74926b046cb49f142125ef5c5919e3fccf
wilbertj8 <wilbertjoseph08@gmail.com>
2022-08-09 12:59:23 -0400

    added useful comments

--- src/visualizer/__init__.py
+++ src/visualizer/__init__.py
@@ -3,6 +3,7 @@ from pickle import TRUE, FALSE
 pygame.init()
 
 class DrawInformation:
+    # colors for use throughout
     BLACK = 0, 0, 0
     WHITE = 255, 255, 255
     GREEN = 0, 255, 0
@@ -16,43 +17,58 @@ class DrawInformation:
         (192, 192, 192)
     ]
     
+    # fonts for use throughout
     FONT = pygame.font.SysFont('calibri', 30)
     LARGE_FONT = pygame.font.SysFont('calibri', 40)
+    # padding top and sides of window
     SIDE_PAD = 100
     TOP_PAD = 150
     
+    # initialize window
     def __init__(self, width, height, lst):
         self.width = width
         self.height = height
         
+        # actually create window using pygame
         self.window = pygame.display.set_mode((width, height))
         pygame.display.set_caption('Sorting Algorithm Visualization')
         self.set_list(lst)
     
+    # helper method to initialize list and its variables
     def set_list(self, lst):
         self.lst = lst
         self.min_val = min(lst)
         self.max_val = max(lst)
         
+        # width of each column is proportional to # of list elements
         self.block_width = round((self.width - self.SIDE_PAD) / len(lst))
+        # height of each column is proportional to # of list elements between max and min
         self.block_height = round((self.height - self.TOP_PAD) / (self.max_val - self.min_val))
+        # start drawing after side pad
         self.start_x = self.SIDE_PAD // 2
 
+# method to draw entire window
 def draw(draw_info):
+    # clear screen
     draw_info.window.fill(draw_info.BACKGROUND_COLOR)
     
+    # initialize controls
     controls = draw_info.FONT.render("R - Reset | SPACE - Start | A - Ascending | D - Descending", 1, draw_info.BLACK)
+    # centralize controls text
     draw_info.window.blit(controls, (draw_info.width/2 - controls.get_width()/2, 5))
     
     sorting = draw_info.FONT.render("I - Insertion Sort | B - Bubble Sort", 1, draw_info.BLACK)
     draw_info.window.blit(sorting, (draw_info.width/2 - sorting.get_width()/2, 35))
     
     draw_list(draw_info)
+    # display changes
     pygame.display.update()
 
+# method to draw list only
 def draw_list(draw_info, color_positions={}, clear_bg=False):
     lst = draw_info.lst
     
+    # used to clear the area encompassing the list only (keep controls)
     if clear_bg:
         clear_rect = (draw_info.SIDE_PAD//2, draw_info.TOP_PAD, 
                       draw_info.width - draw_info.SIDE_PAD, draw_info.height - draw_info.TOP_PAD)
@@ -60,18 +76,23 @@ def draw_list(draw_info, color_positions={}, clear_bg=False):
     
     for i, val in enumerate(lst):
         x = draw_info.start_x + i * draw_info.block_width
+        # draw from top down
         y = draw_info.height - (val - draw_info.min_val) * draw_info.block_height
         
+        # switch colors to differentiate between columns
         color = draw_info.GRADIENTS[i%3]
         
+        # color switching columns differently
         if i in color_positions:
             color = color_positions[i]
         
         pygame.draw.rect(draw_info.window, color, (x, y, draw_info.block_width, draw_info.block_height * val))
     
+    # update if just the list is being updated
     if clear_bg:
         pygame.display.update()
-    
+
+# bubble sort
 def bubble_sort(draw_info, ascending=True):
     lst = draw_info.lst
     
@@ -86,15 +107,18 @@ def bubble_sort(draw_info, ascending=True):
     
     return lst
 
+# implement game
 def main():
     run = True
     clock = pygame.time.Clock()
     
+    # initialize sample data
     n = 50
     min_val, max_val = 0, 100
     lst = random.sample(range(min_val, max_val), n)
     w, h = 800, 600
     
+    # initialize sorting data
     sorting = False
     ascending = False
     
@@ -105,6 +129,7 @@ def main():
         
         draw(draw_info)
         
+        # implement controls based on keyboard input
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
