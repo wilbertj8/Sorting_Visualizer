3b9540f7979aebc115373fdf5e0406ef915b39d7
wilbertj8 <wilbertjoseph08@gmail.com>
2022-08-09 12:46:19 -0400

    added draw list

--- src/visualizer/__init__.py
+++ src/visualizer/__init__.py
@@ -16,6 +16,8 @@ class DrawInformation:
         (192, 192, 192)
     ]
     
+    FONT = pygame.font.SysFont('calibri', 30)
+    LARGE_FONT = pygame.font.SysFont('calibri', 40)
     SIDE_PAD = 100
     TOP_PAD = 150
     
@@ -38,19 +40,51 @@ class DrawInformation:
 
 def draw(draw_info):
     draw_info.window.fill(draw_info.BACKGROUND_COLOR)
+    
+    controls = draw_info.FONT.render("R - Reset | SPACE - Start | A - Ascending | D - Descending", 1, draw_info.BLACK)
+    draw_info.window.blit(controls, (draw_info.width/2 - controls.get_width()/2, 5))
+    
+    sorting = draw_info.FONT.render("I - Insertion Sort | B - Bubble Sort", 1, draw_info.BLACK)
+    draw_info.window.blit(sorting, (draw_info.width/2 - sorting.get_width()/2, 35))
+    
     draw_list(draw_info)
     pygame.display.update()
 
-def draw_list(draw_info):
+def draw_list(draw_info, color_positions={}, clear_bg=False):
     lst = draw_info.lst
+    
+    if clear_bg:
+        clear_rect = (draw_info.SIDE_PAD//2, draw_info.TOP_PAD, 
+                      draw_info.width - draw_info.SIDE_PAD, draw_info.height - draw_info.TOP_PAD)
+        pygame.draw.rect(draw_info.window, draw_info.BACKGROUND_COLOR, clear_rect)
     
     for i, val in enumerate(lst):
         x = draw_info.start_x + i * draw_info.block_width
         y = draw_info.height - (val - draw_info.min_val) * draw_info.block_height
         
         color = draw_info.GRADIENTS[i%3]
-        pygame.draw.rect(draw_info.window, color, (x, y, draw_info.block_width, draw_info.block_height * val))
         
+        if i in color_positions:
+            color = color_positions[i]
+        
+        pygame.draw.rect(draw_info.window, color, (x, y, draw_info.block_width, draw_info.block_height * val))
+    
+    if clear_bg:
+        pygame.display.update()
+    
+def bubble_sort(draw_info, ascending=True):
+    lst = draw_info.lst
+    
+    for i in range(len(lst)-1):
+        for j in range(len(list)-1-i):
+            num1, num2 = lst[j], lst[j+1]
+            
+            if (num1 > num2 and ascending) or (num1 < num2 and not ascending):
+                lst[j], lst[j+1] = lst[j+1], lst[j]
+                # draw_list()
+                yield True
+    
+    return lst
 
 def main():
     run = True
@@ -60,6 +94,9 @@ def main():
     min_val, max_val = 0, 100
     lst = random.sample(range(min_val, max_val), n)
     w, h = 800, 600
+    
+    sorting = False
+    ascending = False
     
     draw_info = DrawInformation(w, h, lst)
     
@@ -71,6 +108,23 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            
+            if event.type != pygame.KEYDOWN:
+                continue
+            
+            if event.key == pygame.K_r:
+                lst = random.sample(range(min_val, max_val), n)
+                draw_info.set_list(lst)
+                sorting = False
+            
+            elif event.key == pygame.K_SPACE:
+                sorting = True
+            
+            elif event.key == pygame.K_a and not sorting:
+                ascending = True
+            
+            elif event.key == pygame.K_d and not sorting:
+                ascending = False
     
     pygame.quit()
 
